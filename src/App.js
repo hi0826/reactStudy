aec73bb779a67a2b2219da58cf738c18a8ff6141
hi0826 <kkkttt555@naver.com>
2022-11-03 14:22:42 +0900

    react study test

--- src/App.js
+++ src/App.js
@@ -55,6 +55,27 @@ function Article(props) {
   );
 }
 
+function MyComponent() {
+  const [count, setCount] = useState(0);
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+  function onClick() {
+    setCount1(count1 + 1);
+    setTimeout(() => {}, 1000);
+    console.log(count1, count2);
+    setCount2(count2 + 1);
+  }
+  const result = count1 >= count2;
+  return (
+    <div>
+      <h2>
+        {count1}, {count2}
+      </h2>
+      <button onClick={onClick}>증가</button>
+    </div>
+  );
+}
+
 function App() {
   const [mode, setMode] = useState("WELCOME");
   const [id, setId] = useState(null);
@@ -93,6 +114,7 @@ function App() {
         }}
       ></Nav>
       {content}
+      <MyComponent />
     </div>
   );
 }
