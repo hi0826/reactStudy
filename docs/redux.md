cc7400b0ba4b0aa9744870a691d8b13439d1e437
hi0826 <kkkttt555@naver.com>
2022-12-06 17:44:26 +0900

    react study, redux

--- docs/redux.md
+++ docs/redux.md
@@ -3,3 +3,15 @@
 - 예시 코드
 - 끝없이 반복되는 컴포넌트로 props를 통해 전달하는 경우 코드가 매우 복잡해진다.
 - `npm i redux react-redux`
+- Provider
+  - store 를 연동하기 위한 컴포넌트
+- useSelector
+  - 어떤 상태 값을 사용할지 선택
+  - 함수를 인자로 받는다.
+- useDispatch
+  - 상태 값을 변경할때 사용
+- connect
+  - HOC(Higher Order Component)패턴
+  - 클래스형 컴포넌트에서 컴포넌트를 재사용할 때 유용한 패턴
+  - 컴포넌트를 특정 함수로 감싸서 특정 값 또는 함수를 props로 받아와서 사용 할 수 있게 해주는 패턴
+  - Hook을 사용하는 지금은 필요 없음
--- src/App2.js
+++ src/App2.js
@@ -1,15 +1,10 @@
 import React, { useState } from 'react';
 import './style.css';
-import { createStore } from 'redux';
+import { regacyStore as createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 // createStore 대신 configureStore 사용
-import {
-  Provide,
-  useSelector,
-  useDispatch,
-  connect,
-  Provider,
-} from 'react-redux';
-// 컴포넌트, 어떤 상태값을 쓸지 선택, 상태값 변경시에 사용, 재사용성을 위할때 필요
+import { Provider, useSelector, useDispatch, connect } from 'react-redux';
+// store 를 연동하기 위한 컴포넌트, 어떤 상태값을 쓸지 선택, 상태값 변경시에 사용, 재사용성을 위할때 필요(클래스형 컴포넌트에서 사용)
 // 리덕스를 사용하면 부모 컴포넌트는 리렌더링되지않고 state가 변경된 컴포넌트만 리렌더링된다.
 // 현재 state, 어떻게 바꿀 것인지 action
 function reducer(currentState, action) {
@@ -26,13 +21,14 @@ function reducer(currentState, action) {
 }
 
 const store = createStore(reducer);
+const configure = configureStore({ reducer: reducer });
 
 export default function App2() {
   return (
     <div id="container">
       <h1>Root</h1>
       <div>
-        <Provider store={store}>
+        <Provider store={configure}>
           <Left1></Left1>
           <Right1></Right1>
         </Provider>
@@ -55,14 +51,14 @@ function Left2(props) {
   return (
     <div>
       <h1>Left2 : </h1>
-      <Left3 number={props.number}></Left3>
+      <Left3></Left3>
     </div>
   );
 }
 
 function Left3(props) {
   console.log('3');
-  // useSelector는 함수를 인자로 방므
+  // useSelector는 함수를 인자로 받음
   const number = useSelector((state) => state.number);
   return (
     <div>
